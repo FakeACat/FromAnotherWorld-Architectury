d30ede0aa09f71be7c4c292747dce665a016a094
acats <33098659+FakeACat@users.noreply.github.com>
2023-06-28 17:51:42 +0100

    Sounds now use the new registry system

--- fabric/src/main/java/acats/fromanotherworld/fabric/registry/SoundRegistryFabric.java
+++ fabric/src/main/java/acats/fromanotherworld/fabric/registry/SoundRegistryFabric.java
@@ -1,17 +1,13 @@
 package acats.fromanotherworld.fabric.registry;
 
-import acats.fromanotherworld.FromAnotherWorld;
 import acats.fromanotherworld.registry.SoundRegistry;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.sounds.SoundEvent;
 
 public class SoundRegistryFabric {
     public static void register(){
-        for (String soundID:
-                SoundRegistry.SOUND_REGISTRY) {
-            Registry.register(BuiltInRegistries.SOUND_EVENT, soundID, SoundEvent.createVariableRangeEvent(new ResourceLocation(FromAnotherWorld.MOD_ID, soundID)));
-        }
+        SoundRegistry.SOUND_REGISTRY.registerAll(
+                (id, registryObject, builder) -> Registry.register(BuiltInRegistries.SOUND_EVENT, id, builder.get())
+        );
     }
 }
--- forge/src/main/java/acats/fromanotherworld/forge/registry/SoundRegistryForge.java
+++ forge/src/main/java/acats/fromanotherworld/forge/registry/SoundRegistryForge.java
@@ -2,7 +2,6 @@ package acats.fromanotherworld.forge.registry;
 
 import acats.fromanotherworld.FromAnotherWorld;
 import acats.fromanotherworld.registry.SoundRegistry;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -12,10 +11,9 @@ public class SoundRegistryForge {
 
     public static final DeferredRegister<SoundEvent> SOUNDS = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, FromAnotherWorld.MOD_ID);
     public static void register(IEventBus eventBus){
-        for (String soundID:
-                SoundRegistry.SOUND_REGISTRY) {
-            SOUNDS.register(soundID, () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(FromAnotherWorld.MOD_ID, soundID)));
-        }
+        SoundRegistry.SOUND_REGISTRY.registerAll(
+                (id, registryObject, builder) -> SOUNDS.register(id, builder)
+        );
         SOUNDS.register(eventBus);
     }
 }

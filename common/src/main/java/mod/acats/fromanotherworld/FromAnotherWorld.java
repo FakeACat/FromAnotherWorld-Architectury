93aa88a8abc100b740926c1d811cc00929214f22
acats <33098659+FakeACat@users.noreply.github.com>
2023-09-04 16:21:25 +0100

    Moved sound registry to FAL

--- common/src/main/java/mod/acats/fromanotherworld/FromAnotherWorld.java
+++ common/src/main/java/mod/acats/fromanotherworld/FromAnotherWorld.java
@@ -9,6 +9,7 @@ import mod.acats.fromanotherworld.config.Config;
 import mod.acats.fromanotherworld.registry.*;
 import mod.azure.azurelib.AzureLib;
 import net.minecraft.core.particles.ParticleType;
+import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
@@ -84,6 +85,11 @@ public class FromAnotherWorld implements CommonMod {
     @Override
     public Optional<FALRegister<ParticleType<?>>> getParticleRegister() {
         return Optional.of(ParticleRegistry.PARTICLE_REGISTRY);
+    }
+
+    @Override
+    public Optional<FALRegister<SoundEvent>> getSoundEventRegister() {
+        return Optional.of(SoundRegistry.SOUND_REGISTRY);
     }
 
     @Override
--- common/src/main/java/mod/acats/fromanotherworld/registry/SoundRegistry.java
+++ common/src/main/java/mod/acats/fromanotherworld/registry/SoundRegistry.java
@@ -1,31 +1,30 @@
 package mod.acats.fromanotherworld.registry;
 
+import mod.acats.fromanotherlibrary.registry.FALRegister;
+import mod.acats.fromanotherlibrary.registry.FALRegistryObject;
 import mod.acats.fromanotherworld.FromAnotherWorld;
 
-import mod.acats.fromanotherworld.utilities.registry.FAWRegister;
-import mod.acats.fromanotherworld.utilities.registry.FAWRegistryObject;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
 
 public class SoundRegistry {
+    public static final FALRegister<SoundEvent> SOUND_REGISTRY = new FALRegister<>();
 
-    public static final FAWRegister<SoundEvent> SOUND_REGISTRY = new FAWRegister<>();
+    public static final FALRegistryObject<SoundEvent> GENERAL_AMBIENT = register("fromanotherworld.entity.thing.general.ambient");
+    public static final FALRegistryObject<SoundEvent> GENERAL_HURT = register("fromanotherworld.entity.thing.general.hurt");
+    public static final FALRegistryObject<SoundEvent> GENERAL_DEATH = register("fromanotherworld.entity.thing.general.death");
 
-    public static final FAWRegistryObject<SoundEvent> GENERAL_AMBIENT = register("fromanotherworld.entity.thing.general.ambient");
-    public static final FAWRegistryObject<SoundEvent> GENERAL_HURT = register("fromanotherworld.entity.thing.general.hurt");
-    public static final FAWRegistryObject<SoundEvent> GENERAL_DEATH = register("fromanotherworld.entity.thing.general.death");
+    public static final FALRegistryObject<SoundEvent> WEAK_AMBIENT = register("fromanotherworld.entity.thing.weak.ambient");
+    public static final FALRegistryObject<SoundEvent> WEAK_HURT = register("fromanotherworld.entity.thing.weak.hurt");
+    public static final FALRegistryObject<SoundEvent> WEAK_DEATH = register("fromanotherworld.entity.thing.weak.death");
+    public static final FALRegistryObject<SoundEvent> WEAK_ALERT = register("fromanotherworld.entity.thing.weak.alert");
 
-    public static final FAWRegistryObject<SoundEvent> WEAK_AMBIENT = register("fromanotherworld.entity.thing.weak.ambient");
-    public static final FAWRegistryObject<SoundEvent> WEAK_HURT = register("fromanotherworld.entity.thing.weak.hurt");
-    public static final FAWRegistryObject<SoundEvent> WEAK_DEATH = register("fromanotherworld.entity.thing.weak.death");
-    public static final FAWRegistryObject<SoundEvent> WEAK_ALERT = register("fromanotherworld.entity.thing.weak.alert");
+    public static final FALRegistryObject<SoundEvent> STRONG_AMBIENT = register("fromanotherworld.entity.thing.strong.ambient");
+    public static final FALRegistryObject<SoundEvent> STRONG_HURT = register("fromanotherworld.entity.thing.strong.hurt");
+    public static final FALRegistryObject<SoundEvent> STRONG_DEATH = register("fromanotherworld.entity.thing.strong.death");
+    public static final FALRegistryObject<SoundEvent> STRONG_ALERT = register("fromanotherworld.entity.thing.strong.alert");
 
-    public static final FAWRegistryObject<SoundEvent> STRONG_AMBIENT = register("fromanotherworld.entity.thing.strong.ambient");
-    public static final FAWRegistryObject<SoundEvent> STRONG_HURT = register("fromanotherworld.entity.thing.strong.hurt");
-    public static final FAWRegistryObject<SoundEvent> STRONG_DEATH = register("fromanotherworld.entity.thing.strong.death");
-    public static final FAWRegistryObject<SoundEvent> STRONG_ALERT = register("fromanotherworld.entity.thing.strong.alert");
-
-    private static FAWRegistryObject<SoundEvent> register(String id){
+    private static FALRegistryObject<SoundEvent> register(String id){
         return SOUND_REGISTRY.register(id, () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(FromAnotherWorld.MOD_ID, id)));
     }
 }

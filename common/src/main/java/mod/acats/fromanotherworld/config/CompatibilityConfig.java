855359b13b677d88f1c8a0decb7ec336ad98ac09
acats <33098659+FakeACat@users.noreply.github.com>
2023-10-07 14:21:40 +0100

    New compat options

--- common/src/main/java/mod/acats/fromanotherworld/config/CompatibilityConfig.java
+++ common/src/main/java/mod/acats/fromanotherworld/config/CompatibilityConfig.java
@@ -10,26 +10,59 @@ public class CompatibilityConfig extends FALConfig {
 
     @Override
     protected int version() {
-        return 0;
+        return 1;
     }
 
-    public final FALConfigBooleanProperty sporeAllies = new FALConfigBooleanProperty(
-            "spore_allies",
-            "Things never fight Fungal Infection mobs?",
-            true
+    /*public final FALConfigIntegerProperty sporeCompatMode = new FALConfigIntegerProperty(
+            "spore_compat_mode",
+            """
+                    Compatibility mode for Spore: Fungal Infection
+                    0 - Spore mobs are allies of The Thing
+                    1 - Spore mobs are attacked by The Thing
+                    2 - Spore mobs can be assimilated by The Thing
+                    You may need to modify the config of Spore: Fungal Infection for some options to work correctly""",
+            0
+    );/*
+
+    /*public final FALConfigIntegerProperty sculkCompatMode = new FALConfigIntegerProperty(
+            "sculk_compat_mode",
+            """
+                    Compatibility mode for Sculk Horde
+                    0 - Sculk Horde mobs are allies of The Thing
+                    1 - Sculk Horde mobs are attacked by The Thing
+                    2 - Sculk Horde mobs can be assimilated by The Thing
+                    You may need to modify the config of Sculk Horde for some options to work correctly""",
+            0
+    );*/
+
+    public final FALConfigBooleanProperty fightFungus = new FALConfigBooleanProperty(
+            "fight_fungus",
+            "Should Things attack mobs from Spore: Fungal Infection?\n" +
+                    "You may need to modify the config of Spore: Fungal Infection for them to fight correctly",
+            false
     );
 
-    public final FALConfigBooleanProperty sculkAllies = new FALConfigBooleanProperty(
-            "sculk_allies",
-            "Things never fight Sculk Horde mobs?",
+    public final FALConfigBooleanProperty fightSculk = new FALConfigBooleanProperty(
+            "fight_sculk",
+            "Should Things attack mobs from Sculk Horde?\n" +
+                    "You may need to modify the config of Sculk Horde for them to fight correctly",
+            false
+    );
+
+    public final FALConfigBooleanProperty xenoAllies = new FALConfigBooleanProperty(
+            "xeno_allies",
+            "Gigeresque Xenomorphs do not attack Things",
             true
     );
 
     @Override
     protected FALConfigProperty<?>[] properties() {
         return new FALConfigProperty[]{
-                this.sporeAllies,
-                this.sculkAllies
+                //this.sporeCompatMode,
+                //this.sculkCompatMode,
+                this.fightFungus,
+                this.fightSculk,
+                this.xenoAllies
         };
     }
 }
--- common/src/main/java/mod/acats/fromanotherworld/registry/DatapackRegistry.java
+++ common/src/main/java/mod/acats/fromanotherworld/registry/DatapackRegistry.java
@@ -1,5 +1,6 @@
 package mod.acats.fromanotherworld.registry;
 
+import mod.acats.fromanotherlibrary.platform.ModLoaderSpecific;
 import mod.acats.fromanotherlibrary.registry.DataPackLoader;
 import mod.acats.fromanotherworld.config.Config;
 
@@ -9,11 +10,37 @@ public class DatapackRegistry {
         loader.addModCompat("guardvillagers");
         loader.addModCompat("mca");
         loader.addModCompat("biomemakeover");
-        if (Config.COMPATIBILITY_CONFIG.sporeAllies.get()) {
-            loader.addModCompat("spore");
+
+        if (ModLoaderSpecific.INSTANCE.isModLoaded("spore")){
+            /*switch (Config.COMPATIBILITY_CONFIG.sporeCompatMode.get()) {
+                case 0 -> loader.addDataPack("spore_alliance");
+                case 1 -> loader.addDataPack("spore_combat");
+                case 2 -> loader.addDataPack("spore_assimilation");
+            }*/
+
+            if (Config.COMPATIBILITY_CONFIG.fightFungus.get()) {
+                loader.addDataPack("spore_combat");
+            }
+            else {
+                loader.addDataPack("spore_alliance");
+            }
         }
-        if (Config.COMPATIBILITY_CONFIG.sculkAllies.get()) {
-            loader.addModCompat("sculkhorde");
+        if (ModLoaderSpecific.INSTANCE.isModLoaded("sculkhorde")) {
+            /*switch (Config.COMPATIBILITY_CONFIG.sculkCompatMode.get()) {
+                case 0 -> loader.addDataPack("sculkhorde_alliance");
+                case 1 -> loader.addDataPack("sculkhorde_combat");
+                case 2 -> loader.addDataPack("sculkhorde_assimilation");
+            }*/
+
+            if (Config.COMPATIBILITY_CONFIG.fightSculk.get()) {
+                loader.addDataPack("sculkhorde_combat");
+            }
+            else {
+                loader.addDataPack("sculkhorde_alliance");
+            }
+        }
+        if (ModLoaderSpecific.INSTANCE.isModLoaded("gigeresque") && Config.COMPATIBILITY_CONFIG.xenoAllies.get()) {
+            loader.addDataPack("gigeresque_alliance");
         }
     }
 }

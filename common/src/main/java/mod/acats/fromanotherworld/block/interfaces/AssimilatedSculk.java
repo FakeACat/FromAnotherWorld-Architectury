34b47fd88b70bd977765e6a72d443578315e974d
acats <33098659+FakeACat@users.noreply.github.com>
2023-10-31 12:34:50 +0000

    Reverted panorama and slowed sculk assimilation

--- common/src/main/java/mod/acats/fromanotherworld/block/interfaces/AssimilatedSculk.java
+++ common/src/main/java/mod/acats/fromanotherworld/block/interfaces/AssimilatedSculk.java
@@ -21,29 +21,32 @@ public interface AssimilatedSculk {
 
     static void assimilateSurroundingSculk(Level level, BlockPos blockPos) {
         BlockUtilities.forEachBlockInCubeCentredAt(blockPos, 1, pos -> {
-            BlockState currentState = level.getBlockState(pos);
-            BlockState assimilatedState;
 
-            if (currentState.is(Blocks.SCULK_VEIN)) {
-                assimilatedState = BlockRegistry.ASSIMILATED_SCULK_VEIN.get().defaultBlockState();
-                for (Map.Entry<Direction, BooleanProperty> entry:
-                     PipeBlock.PROPERTY_BY_DIRECTION.entrySet()) {
-                    BooleanProperty booleanProperty = entry.getValue();
-                    assimilatedState = assimilatedState.setValue(booleanProperty, currentState.getValue(booleanProperty));
+            if (level.getRandom().nextInt(5) == 0) {
+                BlockState currentState = level.getBlockState(pos);
+                BlockState assimilatedState;
+
+                if (currentState.is(Blocks.SCULK_VEIN)) {
+                    assimilatedState = BlockRegistry.ASSIMILATED_SCULK_VEIN.get().defaultBlockState();
+                    for (Map.Entry<Direction, BooleanProperty> entry:
+                            PipeBlock.PROPERTY_BY_DIRECTION.entrySet()) {
+                        BooleanProperty booleanProperty = entry.getValue();
+                        assimilatedState = assimilatedState.setValue(booleanProperty, currentState.getValue(booleanProperty));
+                    }
+                } else if (currentState.is(Blocks.SCULK)) {
+                    assimilatedState = BlockRegistry.ASSIMILATED_SCULK.get().disguisedBlockState();
+                } else if (currentState.is(Blocks.SCULK_SENSOR)) {
+                    assimilatedState = BlockRegistry.ASSIMILATED_SCULK_ACTIVATOR.get().disguisedBlockState();
+                } else if (currentState.is(Blocks.SCULK_SHRIEKER)) {
+                    assimilatedState = BlockRegistry.ASSIMILATED_SCULK_OVERGROWTH.get().disguisedBlockState();
+                } else if (currentState.is(Blocks.SCULK_CATALYST)) {
+                    assimilatedState = BlockRegistry.ASSIMILATED_SCULK_TENTACLES.get().disguisedBlockState();
+                } else {
+                    return;
                 }
-            } else if (currentState.is(Blocks.SCULK)) {
-                assimilatedState = BlockRegistry.ASSIMILATED_SCULK.get().disguisedBlockState();
-            } else if (currentState.is(Blocks.SCULK_SENSOR)) {
-                assimilatedState = BlockRegistry.ASSIMILATED_SCULK_ACTIVATOR.get().disguisedBlockState();
-            } else if (currentState.is(Blocks.SCULK_SHRIEKER)) {
-                assimilatedState = BlockRegistry.ASSIMILATED_SCULK_OVERGROWTH.get().disguisedBlockState();
-            } else if (currentState.is(Blocks.SCULK_CATALYST)) {
-                assimilatedState = BlockRegistry.ASSIMILATED_SCULK_TENTACLES.get().disguisedBlockState();
-            } else {
-                return;
-            }
 
-            level.setBlockAndUpdate(pos, assimilatedState);
+                level.setBlockAndUpdate(pos, assimilatedState);
+            }
         });
     }
 

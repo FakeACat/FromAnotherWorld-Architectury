a02e84814e26f7968773b82ac67a80dac9b4f564
acats <33098659+FakeACat@users.noreply.github.com>
2023-11-03 19:51:22 +0000

    Most blocks are now silk touchable

--- common/src/main/java/mod/acats/fromanotherworld/block/AssimilatedSculkBramblesBlock.java
+++ common/src/main/java/mod/acats/fromanotherworld/block/AssimilatedSculkBramblesBlock.java
@@ -6,14 +6,37 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import org.jetbrains.annotations.Nullable;
 
 @SuppressWarnings("deprecation")
 public class AssimilatedSculkBramblesBlock extends Block {
+    public static final BooleanProperty PERSISTENT = BlockStateProperties.PERSISTENT;
     public AssimilatedSculkBramblesBlock(Properties properties) {
         super(properties);
+        this.registerDefaultState(this.defaultBlockState().setValue(PERSISTENT, false));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(PERSISTENT);
+    }
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext blockPlaceContext) {
+        return this.defaultBlockState().setValue(PERSISTENT, true);
+    }
+
+    @Override
+    public boolean isRandomlyTicking(BlockState blockState) {
+        return super.isRandomlyTicking(blockState) && !blockState.getValue(PERSISTENT);
     }
 
     @Override
